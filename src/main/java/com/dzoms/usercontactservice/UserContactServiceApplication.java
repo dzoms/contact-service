9038bff61e4db28db81d1e8d9e679ea28f439e3a
Vyach12 <slava.gusarov.73@mail.ru>
2024-05-16 23:02:55 +0300

    [feat]: init project

---
+++ src/main/java/com/dzoms/usercontactservice/UserContactServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dzoms.usercontactservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserContactServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserContactServiceApplication.class, args);
+	}
+
+}
